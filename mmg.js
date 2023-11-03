fae9a34274d35d4032464fd15f1ac5bc6715a96a
focusroller <144musagodfrey678@users.noreply.github.com>
2023-11-02 22:22:30 -0700

    first

---
+++ mmg.js
@@ -0,0 +1,30 @@
+function facebook(){
+    alert("will be redirected to your Facebook account")
+}
+function insta(){
+    alert(" will be redirected to your Instagram account")
+}
+function tok(){
+    alert(" will be redirected to your TikTok account")
+}
+function whatsapp(){
+    alert(" will be redirected to WhatsApp chat for your contact number")
+}
+function x(){
+    alert(" will be redirected to your X account")
+}
+function tele(){
+    alert("will be redirected to Telegram chat for your contact number")
+}
+function tube(){
+    alert("will be redirected to your YouTube channel")
+}
+function mail(){
+    alert(" will be redirected to mailing app with your e mail address")
+}
+function call(){
+    alert(" will be redirected to Dialog with your contact")
+}
+function mess(){
+    alert(" will be redirected to message with your contact")
+}
